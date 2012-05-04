6ed0581ae434afa4178d24bea6bbf99ee7f09e5c
Chris St. Pierre <chris.a.st.pierre@gmail.com>
2012-05-04 14:06:35 -0400

    added properties patch

--- src/lib/Server/Plugins/Properties.py
+++ src/lib/Server/Plugins/Properties.py
@@ -8,6 +8,37 @@ import Bcfg2.Server.Plugin
 
 logger = logging.getLogger('Bcfg2.Plugins.Properties')
 
+class Fake(object):
+    """
+    An object that pretends to be an lxml etree node, but that has no children, fake text, and
+    that duplicates itself when any attribute is retrieved or when it is called
+    """
+    def __init__(self, name):
+        self.name = name
+
+    def __getattr__(self, name):
+        if name == 'text':
+            return str(self)
+        else:
+            return Fake('.'.join([self.name, name]))
+
+    def __call__(self, *args, **kwargs):
+        return Fake('%s(args=%s, kwargs=%s)' % (self.name, args, kwargs))
+
+    def __str__(self):
+        return "FAKE: " + self.name
+
+    def __iter__(self):
+        if False:
+            yield None
+        return
+
+    def __getitem__(self, key):
+        return Fake('%s[%s]' % (self.name, key))
+
+    def __len__(self):
+        return 0
+
 class PropertyFile(Bcfg2.Server.Plugin.StructFile):
     """Class for properties files."""
     def write(self):
@@ -73,5 +104,20 @@ class Properties(Bcfg2.Server.Plugin.Plugin,
                               (e.strerror, e.filename))
             raise Bcfg2.Server.Plugin.PluginInitError
 
+        opts = dict(configfile=Bcfg2.Options.CFILE,
+                    default_props=Bcfg2.Options.Option(
+                "Property files to mock out if they don't exist",
+                cmd="--default-property-files", default=[], long_arg=True,
+                cf=('testing', 'default_property_files'),
+                env="DEFAULT_PROPERTY_FILES", cook=Bcfg2.Options.colon_split))
+        setup = Bcfg2.Options.OptionParser(opts)
+        setup.parse(sys.argv[1:], False)
+
+        self.default_property_files = setup['default_props']
+
     def get_additional_data(self, _):
-        return copy.copy(self.store.entries)
+        props = copy.copy(self.store.entries)
+        for fname in self.default_property_files:
+            if fname not in props.keys():
+                props[fname] = Fake(fname)
+        return props
